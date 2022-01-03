6187c7e6dd0ef71ed6a101dfbbcdb645177c3c69
Kevin Shi <kevinshi@kevins-mbp.lan>
2022-01-03 03:14:09 -0600

    capitalized description

--- index.js
+++ index.js
@@ -12,7 +12,15 @@ function weatherBalloon(cityID) {
 function drawWeather(d) {
     var celsius = Math.round(parseFloat(d.main.temp)-273.15);
     var fahrenheit = Math.round(((parseFloat(d.main.temp)-273.15)*1.8)+32);
-    document.getElementById('description').innerHTML = d.weather[0].description;
+    document.getElementById('description').innerHTML = capitalize(d.weather[0].description);
     document.getElementById('temp').innerHTML = fahrenheit + '&deg;';
     document.getElementById('location').innerHTML = d.name;
+}
+
+function capitalize(s) {
+    var words = s.toLowerCase().split(' ');
+    for (var i = 0; i < words.length; i++) {
+       words[i] = words[i].charAt(0).toUpperCase() + words[i].substring(1);
+    }
+    return words.join(' ');
 }
