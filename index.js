4260db1ae9ad3c1dd7a7382a7db92be2a02bf267
Kevin Shi <kevinshi@kevins-mbp.lan>
2022-01-04 14:12:44 -0600

    added API key

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-function weatherBalloon(cityID) {
-    var key = 'a73533782e8c0c25b54a67e88ced663a';
+const weatherBalloon = (cityID) => {
+    var key = config.KEY;
     fetch('https://api.openweathermap.org/data/2.5/weather?id=' + cityID + '&appid=' + key)
-        .then(function(resp) { return resp.json(); })
-        .then(function(data) { drawWeather(data); })
-        .catch(function() {});
+        .then((resp) => { return resp.json(); })
+        .then((data) => { drawWeather(data); })
+        .catch(() => {});
 }
 
-function drawWeather(d) {
+const drawWeather = (d) => {
     var celsius = Math.round(parseFloat(d.main.temp)-273.15);
     var fahrenheit = Math.round(((parseFloat(d.main.temp)-273.15)*1.8)+32);
     document.getElementById('description').innerHTML = capitalize(d.weather[0].description);
@@ -14,7 +14,7 @@ function drawWeather(d) {
     document.getElementById('location').innerHTML = d.name;
 }
 
-function capitalize(s) {
+const capitalize = (s) => {
     var words = s.toLowerCase().split(' ');
     for (var i = 0; i < words.length; i++) {
        words[i] = words[i].charAt(0).toUpperCase() + words[i].substring(1);
